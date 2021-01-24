a7f0425e8ceeb3e80ac798969597050ca01abb9a
federico <federicoadegiovanni@gmail.com>
2021-01-23 23:30:47 -0300

    fix endpoint

--- meli/frontend-next/config.js
+++ meli/frontend-next/config.js
@@ -1,2 +1,3 @@
-export const endpoint = `http://localhost:4444`;
+//export const endpoint = `http://localhost:4444`;
+export const endpoint = `https://fdegiovanni-meli-backend.herokuapp.com`
 export const perPage = 4;
